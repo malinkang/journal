e9667375d753d1562da0ef738f9dbd304ce78b10
malinkang <malinkang@ushareit.com>
2021-09-04 21:54:53 +0800

    update

--- scripts/generator.py
+++ scripts/generator.py
@@ -18,17 +18,24 @@ def getWeekDay():
     today = datetime.now().weekday()
     return week_day_dict[today]
 
-def createDiary(secret,pageId,version):
+def createDiary(secret,pageId,version,cover):
     headers = {'Authorization': secret,"Notion-Version":version}
-    cover = "https://images.unsplash.com/photo-1529963183134-61a90db47eaf?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDMwfDZzTVZqVExTa2VRfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
     title = time.strftime("%m月%d日 星期"+getWeekDay(), time.localtime()) 
     body = {"parent":{"type":"page_id","page_id":pageId},"properties":{"title":{"title":[{"type":"text","text":{"content":title}}]}},"cover":{"type":"external","external":{"url":cover}},"icon":{"type":"emoji","emoji":"🌚"},"children":[{"type":"heading_2","heading_2":{"text":[{"type":"text","text":{"content":"工作"}}]}},{"type":"heading_2","heading_2":{"text":[{"type":"text","text":{"content":"学习"}}]}},{"object":"block","type":"to_do","to_do":{"text":[{"type":"text","text":{"content":"跑步5km🍅"}}],"checked":False}}]}
     r = requests.post('https://api.notion.com/v1/pages/',headers=headers,json=body)
+
+def getCover(accessKey,secret,pageId,version):
+    params = {"client_id":accessKey,"orientation":"landscape"}
+    r = requests.get('https://api.unsplash.com/photos/random',params=params)
+    cover = r.json().get("urls").get("full")
+    createDiary(secret,pageId,version,cover)
+    
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("secret")
     parser.add_argument("id")
     parser.add_argument("version")
+    parser.add_argument("accessKey")
     options = parser.parse_args()
-    createDiary(options.secret, options.id,options.version)
+    getCover(options.accessKey,options.secret, options.id,options.version)
