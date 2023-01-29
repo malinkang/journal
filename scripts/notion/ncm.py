10278e437049357107512d196bc8ac4a878544a3
malinkang <malinkang@ushareit.com>
2023-01-29 16:09:21 +0800

    fix music

--- scripts/notion/ncm.py
+++ scripts/notion/ncm.py
@@ -12,7 +12,6 @@ d = feedparser.parse("https://rsshub.app/ncm/playlist/7648872348")
 for entry in d.entries:
     filter = {"property": "id", "rich_text": {"equals": entry.id}}
     response = notion_api.query_database("46beb49d60b84317a0a2c36a0a024c71", filter)
-    print(len(response.get("results")))
     if(len(response.get("results")) == 0):
         date = datetime.strptime(entry['published'],'%a, %d %b %Y %H:%M:%S %Z')+timedelta(hours=8)
         properties = (
@@ -26,9 +25,8 @@ for entry in d.entries:
             Page()
             .parent(DatabaseParent("46beb49d60b84317a0a2c36a0a024c71"))
             .cover(unsplash.random())
-            .icon("💰")
+            .icon("🎵")
             .children(Children())
             .properties(properties)
         )
         notion_api.create_page(page)
-        print("插入成功")   
