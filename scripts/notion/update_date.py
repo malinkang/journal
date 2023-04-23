e12635590146640a8d9539b0814644ed05bfe036
malinkang <malinkang@ushareit.com>
2023-04-23 15:07:01 +0800

    fix telegram

--- scripts/notion/update_date.py
+++ scripts/notion/update_date.py
@@ -1,9 +1,10 @@
-#更新年
+# 更新年
 import notion_api
 from config import (
     BILL_DATABASE_ID,
     BOOK_DATABASE_ID,
     MOVIE_DATABASE_ID,
+    WEREAD_DATABASE_ID,
 )
 from util import get_date
 from util import get_title
@@ -14,18 +15,25 @@ from datetime import datetime
 dict = {
     # BILL_DATABASE_ID:"日期",
     # BOOK_DATABASE_ID:"打分日期",
-    MOVIE_DATABASE_ID:"打分日期",
+    # MOVIE_DATABASE_ID:"打分日期",
+    WEREAD_DATABASE_ID: "Date",
 }
-def query_year(id,name):
-    filter = {"property":"Year","relation":{"is_empty":True}}
-    response = notion_api.query_database(id,filter)
+
+
+def query_year(id, name):
+    filter = {
+        "and": [
+            {"property": "Year", "relation": {"is_empty": True}},
+            {"property": name, "date": {"is_not_empty": True}},
+        ]
+    }
+    response = notion_api.query_database(id, filter)
     for result in response["results"]:
-        print(get_title(result,"标题"))
-        date = datetime.fromisoformat(get_date(result,name))
-        properties = get_relation(Properties(),date)
-        print(properties)
-        notion_api.update_page(result["id"],properties)
+        date = datetime.fromisoformat(get_date(result, name))
+        properties = get_relation(Properties(), date)
+        notion_api.update_page(result["id"], properties)
+
 
 if __name__ == "__main__":
-    for key,value in dict.items():
-        query_year(key,value)
+    for key, value in dict.items():
+        query_year(key, value)
