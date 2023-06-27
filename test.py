1d125c88174d8fb9a68e6f11fd96c60318200934
malinkang <malinkang@ushareit.com>
2023-06-27 19:51:41 +0800

    add gallery

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-import json
-import math
-
-print(math.ceil(10/3))
+import glob
+import os
+file = glob.glob("./content/posts/*.md")
+for i in file:
+    dir = i[i.rfind("/")+1:i.rfind(".")]
+    os.mkdir(f"./content/posts/{dir}")
+    os.rename(i,f"./content/posts/{dir}/index.md")
