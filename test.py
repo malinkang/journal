7ab9f660faa6e26548c16f6fd1dc457405534081
malinkang <malinkang@ushareit.com>
2023-06-28 12:00:37 +0800

    remove images

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import glob
 import os
-file = glob.glob("./content/posts/2022/*/images/*.png")
+file = glob.glob("./content/posts/2022/*/images/*.jpeg")
 for i in file:
     if os.path.isfile(i):
         os.remove(i)
